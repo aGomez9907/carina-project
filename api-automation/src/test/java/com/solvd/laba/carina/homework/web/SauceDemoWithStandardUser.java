930a1a05669d6f24b264ec4df94eae14d06c4715
agomez <agomez_@live.com.ar>
2023-03-10 12:14:36 -0300

    Everything working again

--- api-automation/src/test/java/com/solvd/laba/carina/homework/web/SauceDemoWithStandardUser.java
+++ api-automation/src/test/java/com/solvd/laba/carina/homework/web/SauceDemoWithStandardUser.java
@@ -30,7 +30,6 @@ public class SauceDemoWithStandardUser implements IAbstractTest {
     @Test()
     @MethodOwner(owner = "agomez")
     public void testItems() {
-
         InventoryPage inventoryPage = new InventoryPage(getDriver());
         InventoryItemPage inventoryItemPage = inventoryPage.selectItemByName("Sauce Labs Onesie");
         inventoryItemPage.assertPageOpened();
